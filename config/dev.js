a71e68d9259b42c2537c072257bee84665d1d868
JhonasV <jhonas@gmail.com>
2019-09-28 19:55:53 -0400

    added prod keys file

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,3 @@
-const db_credentials = {
-  user_db: "db_user",
-  password_db: "hola1234",
-  collection_db: "voteapp"
-};
-
 module.exports = {
-  // DATABASE_URI: `mongodb://${db_credentials.user_db}:${db_credentials.password_db}@ds139632.mlab.com:39632/${db_credentials.collection_db}`
   DATABASE_URI: "mongodb://127.0.0.1:27017/voteApp"
 };
