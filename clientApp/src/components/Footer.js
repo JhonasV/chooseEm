2f8d50cf76b80a5e91e6f7d54c97bd3c1869900f
JhonasV <jhonas@gmail.com>
2019-09-29 11:37:39 -0400

    text in footer modified to h5

--- clientApp/src/components/Footer.js
+++ clientApp/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer className={`footer text-center py-2 bg-primary-custom mt-5`}>
     <div className="footer-copyright ">
-      <h4 className="mb-0">
+      <h5 className="mb-0">
         Made with <i className={`fa fa-heart text_red`} aria-hidden="true"></i>{" "}
         by{" "}
         <a
@@ -13,7 +13,7 @@ const Footer = () => (
         >
           Jhonas Veras
         </a>
-      </h4>
+      </h5>
     </div>
   </footer>
 );
